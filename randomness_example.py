6c5723e6ba234f9dfb0d73f4ea713a980f39aeda
Max Maass <mmaass@seemoo.tu-darmstadt.de>
2018-04-19 16:33:25 +0200

    Refactor to use parameters instead of global vars
    This commit refactors the code to move away from global variables and use
    parameters instead. This makes it much easier to use it in systems that
    keep a persistent state of variables, like Jupyter notebooks.

--- randomness_example.py
+++ randomness_example.py
@@ -1,32 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import codecs
 import sys
 import random
 import os
 
 import matplotlib.pyplot as plt
-import matplotlib.mlab as mlab
+import matplotlib
 import numpy as np
+from scipy.misc import comb
+from cycler import cycler
 
 # create output-dir, if not already existing
 if not os.path.isdir('figures'):
     os.makedirs('figures')
-    
 
-# data structures used for data storage later on.
-sum_distribution = []
 
-transition_probs = {x: 0 for x in range(0, 128)}
-transition_count = [0]
-
-# generate some random data
-keys = [ format(random.SystemRandom().randint(0,2**128),'#0128b')[2:] for x in range(0,1411)]
-
-def random_walk(bits):
+def random_walk(bits, sum_distribution, transition_count, transition_probs):
     ''' takes string of '0' and '1' and turns them into random walks in a galton board. Effectively computes
-	the cumulative sums distribution for every prefix of the input string. '''
+    the cumulative sums distribution for every prefix of the input string. '''
+
     transition_count[0] += 1
     ret = [0]
     for i, b in enumerate(bits[:128]):
@@ -37,9 +30,10 @@ def random_walk(bits):
         ret.append(ret[-1] + val)
 
     sum_distribution.append(ret[-1])
+    return (sum_distribution, transition_count, transition_probs)
 
 
-def markov_transitions():
+def markov_transitions(transition_probs, transition_count):
     ''' transition probabilities from every nth to (n+1)th bit. '''
     norm_transition_probs = [transition_probs[x] / transition_count[0]
                              for x in range(0, 128)]
@@ -48,17 +42,17 @@ def markov_transitions():
     plt.ylabel('Probability for 1')
     plt.ylim([0, 1])
     plt.xlim([0, 128])
-    plt.plot(norm_transition_probs,color='b')
-    plt.savefig('figures/example_markov.png',dpi=300,orientation='landscape')
+    plt.plot(norm_transition_probs, color='b')
+    plt.savefig('figures/example_markov.png', dpi=300, orientation='landscape')
 
-from scipy.misc import comb
+
 
 def binomial_plot(n=256):
     ''' Theoretical binomial distribution that is plotted as red line into figure.'''
     x = np.arange(256)
     y = list(map(lambda xi: comb(256,xi)/2**n, x))
     x = list(map(lambda r: r-2*63.25, x))
-    plt.plot(x,y,color='r')
+    plt.plot(x, y, color='r')
 
 #def binomial_plot(bins,n):
 #    variance =  n * 0.5 * 0.5
@@ -66,7 +60,7 @@ def binomial_plot(n=256):
 #    y = mlab.normpdf(np.asarray(list(range(-128,128))), 0, sigma)
 #    plt.plot(np.asarray(list(range(-128,128))),y,color='r')
 
-def distribution():
+def distribution(sum_distribution):
     ''' Plots the cumulative sums distribution and saves figure. '''
     plt.clf()
     count, bins, ignored = plt.hist(sum_distribution, color='#007a9b', range=(-128,128), normed=True,rwidth=0.5, bins=128)
@@ -75,11 +69,13 @@ def distribution():
     plt.savefig('figures/example_distribution.svg',dpi=300,orientation='landscape')
 
 
-def plot_rand_walk():
+def plot_rand_walk(keys):
     ''' turns string consisting of '0' and '1' into random walks. While the walks are currently not plotted, the distribution
-	of the cumulative sums along with the markov transitions are computed and plotted using them. results are currently
-	passed via vars in scope.'''
-    from cycler import cycler
+    of the cumulative sums along with the markov transitions are computed and plotted using them.'''
+    sum_distribution = []
+    transition_count = [0]
+    transition_probs = {x: 0 for x in range(0, 128)}
+
     matplotlib.rcParams['axes.prop_cycle'] = cycler('color',
                                                     ['#e78a33', '#eda766', '#8d4959', '#aa7782', '#bdcd61', '#cdda89',
                                                      '#8a9c33', '#a7b566'])
@@ -93,15 +89,14 @@ def plot_rand_walk():
 
     plt.ylim([-128, 128])
     for key in keys:
-        random_walk(key)
+        sum_distribution, transition_count, transition_probs = random_walk(key, sum_distribution, transition_count, transition_probs)
     plt.tight_layout()
-    distribution()
-    markov_transitions()
+    distribution(sum_distribution)
+    markov_transitions(transition_probs, transition_count)
 
 
-def plot_heat_map(pt=plt):
-    ''' creates heatmap of random walks. Uses the 'keys' var
-	in scope as input.'''
+def plot_heat_map(keys, pt=plt):
+    ''' creates heatmap of random walks.'''
     intensity = 1
     heatmap_array = []
     for key in keys:
@@ -136,18 +131,14 @@ def plot_heat_map(pt=plt):
     pt.plot(minus_x, minus_y, color='r')
 
 
-def apply_plot_heat_map():
+def apply_plot_heat_map(keys):
     ''' Calls plot_heat_map() and additionally saves the plotted figure.'''
-    plot_heat_map()
+    plot_heat_map(keys)
     plt.savefig('figures/heatmapExample.png', dpi=300, orientation='landscape')
 
 
 if __name__ == "__main__":
-    # layout stuff
-    import matplotlib
-    import matplotlib.pyplot as plt
-
-    from matplotlib.ticker import FuncFormatter
+    keys = [ format(random.SystemRandom().randint(0,2**128),'#0128b')[2:] for x in range(0,1411)]
 
     matplotlib.rc('text', usetex=True)
     matplotlib.rc('font', family='serif')
@@ -160,6 +151,6 @@ if __name__ == "__main__":
 
     # argument handling
     if sys.argv[1] == 'heatmap':
-        apply_plot_heat_map()
+        apply_plot_heat_map(keys)
     elif sys.argv[1] == 'rand':
-        plot_rand_walk()
+        plot_rand_walk(keys)
